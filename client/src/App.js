3efc9aa8660f3fd8e28d6f04b4de08b4ad69726f
Jacob Walton <jawalton6616@gmail.com>
2021-03-18 12:17:03 -0500

    styled login component

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,20 @@ function App() {
 
   return (
     <div className="App">
-      {!loggedIn ? <h1>Not logged in!</h1> : <h1>Logged in</h1>}
+      {!loggedIn ? (
+        <div className="logIn">
+          <h1 className="header">React Chat</h1>
+          <br />
+          <div className="logInInputs">
+            <input type="text" placeholder="Name..." />
+            <input type="text" placeholder="Room..." />
+          </div>
+
+          <button>Enter Chat</button>
+        </div>
+      ) : (
+        <h1>Logged in</h1>
+      )}
       <h1></h1>
     </div>
   );
