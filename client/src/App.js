94cae45e964750c6056a6488c5ebb88d1e706acb
Jacob Walton <jawalton6616@gmail.com>
2021-03-18 11:38:42 -0500

    set up rendering logic dependent upon if user is logged in or not

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [loggedIn, setloggedIn] = useState(false);
+
+  return (
+    <div className="App">
+      {!loggedIn ? <h1>Not logged in!</h1> : <h1>Logged in</h1>}
+      <h1></h1>
+    </div>
+  );
 }
 
 export default App;
